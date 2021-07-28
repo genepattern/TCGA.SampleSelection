ea0494dcd51c8ecd103f27be7959531d6f3f9092
bahill <bhill@broadinstitute.org>
2021-07-28 11:19:52 -0400

    removing quiet from all installs for debugging

--- src/installPkgs.R
+++ src/installPkgs.R
@@ -6,7 +6,7 @@ suppressMessages(suppressWarnings(install.packages("/TCGA_SS/optparse_1.6.6.tar.
 # suppressMessages(suppressWarnings(install.packages("/TCGA_SS/dplyr_1.0.7.tar.gz")))
 suppressMessages(suppressWarnings(install.packages("/TCGA_SS/BiocManager_1.30.15.tar.gz")))
 
-suppressMessages(suppressWarnings(BiocManager::install("cBioPortalData", version = "3.12", ask = FALSE, quiet = TRUE)))
+suppressMessages(suppressWarnings(BiocManager::install("cBioPortalData", version = "3.12", ask = FALSE)))
 # 2.2.11, from Bioconductor v3.12
-suppressMessages(suppressWarnings(BiocManager::install("AnVIL", version = "3.12", ask = FALSE, quiet = TRUE)))
+suppressMessages(suppressWarnings(BiocManager::install("AnVIL", version = "3.12", ask = FALSE)))
 # 1.2.0, from Bioconductor v3.12
